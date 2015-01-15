7b68e9745455c12b7e5ecd69e917470e473a5836
Ara Hacopian <ara@smartlogic.io>
2015-01-15 00:37:26 -0500

    Implement name on Person using composed_of

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,17 +1,13 @@
 class Person < ActiveRecord::Base
-  def name
-    @name ||= Name.new(title, first_name, middle_name, last_name, suffix)
-  end
-
-  def name=(other_name)
-    self.title = other_name.title
-    self.first_name = other_name.first
-    self.middle_name = other_name.middle
-    self.last_name = other_name.last
-    self.suffix = other_name.suffix
-
-    @name = other_name
-  end
+  composed_of :name,
+    allow_nil: true,
+    mapping: {
+      title: :title,
+      first_name: :first,
+      middle_name: :middle,
+      last_name: :last,
+      suffix: :suffix
+    }
 
   def weight
     @weight ||= Weight.new(weight_value)
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Person, :type => :model do
-  it 'serializes and deserializes name' do
-    name = Name.new('Mr.', 'First', 'Middle', 'Last', 'Jr.')
-    described_class.create(name: name)
+  describe 'name' do
+    let(:name) { Name.new('Mr.', 'First', 'Middle', 'Last', 'Jr.') }
 
-    person = Person.last
-    expect(person.name).to eq(name)
+    it 'serializes and deserializes name' do
+      described_class.create(name: name)
+
+      person = Person.last
+
+      expect(person.name).to eq(name)
+    end
+
+    it 'can find a person by name' do
+      person = described_class.create(name: name)
+
+      expect(described_class.find_by(name: name)).to eq(person)
+    end
   end
 
   it 'serializes and deserializes weight' do
